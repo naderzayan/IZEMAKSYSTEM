a071137b7dc2ee47d2922d61fa20b080d3ae6a0d
naderzayan <naderahmed.na49@gmail.com>
2025-11-09 16:39:05 +0200

    QrCode

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Login from './pages/Login'
-import CreateNewParty from './pages/CreateNewParty'
-import MainPartyData from './pages/MainPartyData'
-import AddInvitors from './pages/AddInvitors'
-import InvitorsPage from './pages/InvitorsPage'
-import UpDateInvitor from './pages/UpDateInvitor'
-import QRCodeScanner from './pages/QRCodeScanner'
-import DeletedParties from './pages/DeletedParties'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+// لو عايز تستخدم HashRouter بدل BrowserRouter (لو السيرفر مش بيعمل rewrite للـ SPA)
+// استبدل السطرين فوق بالسطر ده:
+// import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+import Login from "./pages/Login";
+import CreateNewParty from "./pages/CreateNewParty";
+import MainPartyData from "./pages/MainPartyData";
+import AddInvitors from "./pages/AddInvitors";
+import InvitorsPage from "./pages/InvitorsPage";
+import UpDateInvitor from "./pages/UpDateInvitor";
+import QRCodeScanner from "./pages/QRCodeScanner";
+import DeletedParties from "./pages/DeletedParties";
 
 export default function App() {
   return (
     <main>
       <Router>
         <Routes>
-          <Route path='/' element={<Login />}></Route>
-          <Route path='/login' element={<Login />}></Route>
-          <Route path='/createnewparty' element={<CreateNewParty />}></Route>
-          <Route path='/mainpartydata' element={<MainPartyData />}></Route>
+          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/createnewparty" element={<CreateNewParty />} />
+          <Route path="/mainpartydata" element={<MainPartyData />} />
           <Route path="/deletedparties" element={<DeletedParties />} />
-          <Route path='/addinvitors' element={<AddInvitors />}></Route>
-          <Route path='/invitorspage' element={<InvitorsPage />}></Route>
-          <Route path='/updateinvitor' element={<UpDateInvitor />}></Route>
-          <Route path='/qr_code_scanner' element={<QRCodeScanner />}></Route>
+          <Route path="/addinvitors" element={<AddInvitors />} />
+          <Route path="/invitorspage" element={<InvitorsPage />} />
+          <Route path="/updateinvitor" element={<UpDateInvitor />} />
+          <Route path="/qr_code_scanner" element={<QRCodeScanner />} />
+          <Route path="/ar_qr_code_scanner" element={<QRCodeScanner />} />
         </Routes>
       </Router>
     </main>
-  )
+  );
 }
